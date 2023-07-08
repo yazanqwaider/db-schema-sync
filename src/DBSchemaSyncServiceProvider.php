18c1bed38819895ee3275a821128f38f8aa29dd8
yazanQwaider <yazeno15@gmail.com>
2023-07-08 17:59:17 +0300

    first commit

---
+++ src/DBSchemaSyncServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+ 
+namespace DBSchemaSync;
+ 
+use Illuminate\Support\ServiceProvider;
+ 
+class DBSchemaSyncServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        # code...
+    }
+ 
+    public function register()
+    {
+        # code...
+    }
+}
